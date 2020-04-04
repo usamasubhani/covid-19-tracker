fd8d3a28036d114d590f125e3ab2d6268f48c7b5
Usama Subhani <reachusama@protonmail.com>
2020-04-04 17:20:12 +0500

    Add commas in numbers

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,9 +1,9 @@
 <template>
     <b-container class="bv-example-row">
         <b-row v-if="data != null">
-            <b-col><h1>{{ data.cases || data.confirmed }}</h1>Cases </b-col>
-            <b-col><h1>{{ data.deaths }}</h1>Deaths </b-col>
-            <b-col><h1>{{ data.recovered }}</h1>Recovered </b-col>
+            <b-col><h1 v-if="data.cases" >{{ data.cases.toLocaleString('en') }}</h1> <h1 v-else>{{data.confirmed.toLocaleString('en') }}</h1>Cases </b-col>
+            <b-col><h1>{{ data.deaths.toLocaleString('en') }}</h1>Deaths </b-col>
+            <b-col><h1>{{ data.recovered.toLocaleString('en') }}</h1>Recovered </b-col>
         </b-row>
     </b-container>
 </template>
